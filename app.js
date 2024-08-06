7f53433b8954f3059a90547db09166fe7d463019
finteger <nashassociatesinc@gmail.com>
2024-08-06 10:45:01 -0600

    My Third Commit

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 const express = require('express');
-const ejs = require('ejs');
-
 const app = express();
+
 const PORT = 3000;
 
-
-//setting our view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
-//route controller with a path paremeter
-app.get('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    res.send(`User with ID ${userId}`);
-});
-
+//Query Parameters
 app.get('/', (req, res) =>{
 
-    const data = {
-        title: 'Hello, World!',
-        message: 'Welcome to my website!'
-    }
+    const id = req.query.id;
+    const username = req.query.username;
 
-   res.render('index', {data});
+    res.send(`User ID: ${id}.  Username: ${username}`);
+});
+
+app.get('/products/:id', (req, res) =>{
+
+  const productId = req.params.id;
+
+  const product = [
+    {"id": 1, "name" : "Product A"},
+    {"id": 2, "name" : "Product B"},
+    {"id": 3, "name" : "Product C"},
+  ]
 
 });
 
+
+
 app.listen(PORT, () => {
-    console.log(`Server stard on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
