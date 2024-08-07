283e0de0b052cb4b4e8e5e165cdc63bdf1fd815a
finteger <nashassociatesinc@gmail.com>
2024-08-07 11:10:46 -0600

    Commit

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 8000;
 
 
-app.use(express.static('public'));
 
-//middleware
-app.use((req, res, next)=>{
-  console.log(`Request received: ${req.method} ${req.url}`);
-  next();
-});
 
-//router controller
-app.get('/', (req, res) =>{
-  res.send(`Hello World!`);
-});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
