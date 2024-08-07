8ca3bc07638a850c668577f5378bf78da91d15bf
finteger <nashassociatesinc@gmail.com>
2024-08-07 11:32:43 -0600

    Commit

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
 
+//serving static files
+app.use(express.static('public'));
 
+//setup our views
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
+//router handler
+app.get('/', (req, res) =>{
+   res.render('home');
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
