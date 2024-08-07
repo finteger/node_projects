059afb8385a49c7911365f6bc4297eb13cc27165
finteger <nashassociatesinc@gmail.com>
2024-08-07 09:56:35 -0600

    My Whatever Update

--- app.js
+++ app.js
@@ -3,33 +3,19 @@ const app = express();
 
 const PORT = 3000;
 
-//Query Parameters
+
+app.use(express.static('public'));
+
+//middleware
+app.use((req, res, next)=>{
+  console.log(`Request received: ${req.method} ${req.url}`);
+  next();
+});
+
+//router controller
 app.get('/', (req, res) =>{
-
-    const id = req.query.id;
-    const username = req.query.username;
-
-    res.send(`User ID: ${id}.  Username: ${username}`);
+  res.send(`Hello World!`);
 });
-
-app.get('/products/:id', (req, res) =>{
-
-  const productId = req.params.id;
-
-  const products = [
-    {"id": 1, "name" : "Product A"},
-    {"id": 2, "name" : "Product B"},
-    {"id": 3, "name" : "Product C"},
-  ]
-
-  const product = products.find(product => product.id == parseInt(productId));
-
-  res.send(`Product id: ${product.id}.  Product Name: ${product.name}`);
-
-
-});
-
-
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
